a073e6afc4c2fb290f23631985051b2b248d627f
dilusha07 <dilushasandaruwani07@gmail.com>
2024-07-11 23:50:20 +0530

    Connect MongoDB cloud database

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(express.json());
 
 const port = 3000;
+
+//Connect MongoDB cloud database
+const DB_URL = "mongodb+srv://Test1:Test1@cluster1.lshemxo.mongodb.net/TestAPP?retryWrites=true&w=majority";
+  
+  mongoose.connect(DB_URL)
+  .then(()=>{
+      console.log('DB connected!');
+  })
+  .catch((err)=>console.log("Db connected faild!", err));
+
 
 // Start server
 app.listen(port, () => {
